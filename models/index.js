d665945f83fbcc397002afbaaccd59687523e9e2
Seth Torek <seth.torek@gmail.com>
2021-04-22 22:46:53 -0400

    finished associations

--- models/index.js
+++ models/index.js
@@ -5,10 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {through: ProductTag});
+Tag.belongsToMany(Product, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
 
